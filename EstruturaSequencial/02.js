a5fbf1590c386ece4d0bc831668522b3c5d5b670
ferreirascdb <cruzrikjr@hotmail.com>
2025-04-06 19:36:42 -0300

    Listas Python Brasil

---
+++ EstruturaSequencial/02.js
@@ -0,0 +1,9 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Digite um número: ', (numero) => {
+    console.log('O número informado foi ' + numero);
+    readline.close();
+});
